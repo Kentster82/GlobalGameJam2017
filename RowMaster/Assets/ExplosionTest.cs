7adc9d4138142e441ef53c2529655417f6564da8
xEmptyPockets <mcrober1@uci.edu>
2017-01-21 14:54:11 -0800

    Testing explosions for wave physics
    Trying out AddExplosionForce for wave physics, though it doesn't feel
    satisfying atm and is too strong against things that are far from the
    origin point

---
+++ RowMaster/Assets/ExplosionTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionTest : MonoBehaviour {	
+	// Update is called once per frame
+	private int frameCount = 0;
+	public GameObject boat1;
+	public GameObject boat2;
+
+	void Update () {
+		frameCount += 1;
+		if(frameCount >= 600){
+			boat1.GetComponent<Rigidbody> ().AddExplosionForce (1000.0f, transform.position, 25f, 3f);
+			boat2.GetComponent<Rigidbody> ().AddExplosionForce (1000.0f, transform.position, 25, 3f);
+			frameCount = 0;
+		}
+	}
+}
