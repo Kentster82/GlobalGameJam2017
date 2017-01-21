b05b75cce78e2e429e78575bffd3094ff767375e
Kent Pluntze <Kpluntze@uci.edu>
2017-01-21 10:31:31 -0800

    Boat Movement

---
+++ RowMaster/Assets/scripts/row_row_row_ya_boat.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class row_row_row_ya_boat : MonoBehaviour {
+    Vector3 direction;
+    public int force = 5;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction = transform.Find("Front").position - transform.position;
+            direction = force *direction.normalized;
+            //Debug.Log(direction);
+            GetComponent<Rigidbody>().AddForceAtPosition(direction, transform.Find("Right").position);
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = transform.Find("Front").position - transform.position;
+            direction = force * direction.normalized;
+            //Debug.Log(direction);
+            GetComponent<Rigidbody>().AddForceAtPosition(-direction, transform.Find("Right").position);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction = transform.Find("Front").position - transform.position;
+            direction = force * direction.normalized;
+            //Debug.Log(direction);
+            GetComponent<Rigidbody>().AddForceAtPosition(direction, transform.Find("Left").position);
+
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction = transform.Find("Front").position - transform.position;
+            direction = force * direction.normalized;
+            //Debug.Log(direction);
+            GetComponent<Rigidbody>().AddForceAtPosition(-direction, transform.Find("Left").position);
+
+        }
+    }
+    void Update () {
+      
+	}
+}
