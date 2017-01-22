6a68905405c832370cf75a3b248a5b1f2586faee
DJ <unodeej@gmail.com>
2017-01-22 13:51:39 -0800

    Added playSplash func in camperSounds script

---
+++ RowMaster/Assets/scripts/camperSounds.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camperSounds : MonoBehaviour {
+
+	void playSplash()
+    {
+        Debug.Log("SPLASH");
+    }
+}
