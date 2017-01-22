c04493359a56b1ab34d47a7165fc0449adbd5e05
DJ <unodeej@gmail.com>
2017-01-21 22:10:32 -0800

    Pimpin Game GUI script
    Framework for displaying score values on screen. Change P1Text and
    P2Text as needed.

--- RowMaster/Assets/Pimpin.cs
+++ RowMaster/Assets/Pimpin.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Pimpin : MonoBehaviour {
+    public int offset1 = 13;
+    public int offset2 = 72;
 
-	// Use this for initialization
-	void Start () {
+    string P1Text = "P1 SCORE";
+    string P2Text = "P2 SCORE";
+
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -13,4 +19,10 @@ public class Pimpin : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(offset1*Screen.width / 100, 20, Screen.width / 4, 20), P1Text);
+        GUI.Label(new Rect(offset2*Screen.width / 100, 20, Screen.width, 20), P2Text);
+    }
 }
