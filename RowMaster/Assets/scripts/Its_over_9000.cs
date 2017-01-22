c1933ddb998a369537642b2338e4da5a835a3cfb
Kent Pluntze <Kpluntze@uci.edu>
2017-01-21 23:10:09 -0800

    Unlimited POOOOOWA

--- RowMaster/Assets/scripts/Its_over_9000.cs
+++ RowMaster/Assets/scripts/Its_over_9000.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Its_over_9000 : MonoBehaviour {
     int time, time2,count;
     float angle, angleprev,anglesum;
-    bool NE,NW,SE,SW, NE2, SE2, NW2, SW2;
-    Vector3 last,first, check=Vector3.forward;
+    //bool NE,NW,SE,SW, NE2, SE2, NW2, SW2;
+    bool POWER;
+    Vector3 last, first;
     float[] Angles, Deltas; 
 	// Use this for initialization
 	void Start () {
+        POWER = false;
         Angles = new float[300];
         Deltas = new float[300];
         time = 0;
@@ -63,7 +65,8 @@ public class Its_over_9000 : MonoBehaviour {
             //Debug.Log(angleprev);
             if ( anglesum + (angleprev-(time/2)) >= 360)
             {
-                GetComponent<Rigidbody>().MovePosition(new Vector3(0, 5, 0));
+                //GetComponent<Rigidbody>().MovePosition(new Vector3(0, 5, 0));
+                POWER = true;
                 count = 0;
                 time = 0;
                 angle = 0;
@@ -83,28 +86,32 @@ public class Its_over_9000 : MonoBehaviour {
         
     }
     // Update is called once per frame
-    void setbools(Vector3 V)
-    {
-        if (V.x > 0 && V.z > 0)
-        {
-            NE = true;
-        }
-        else if (V.x > 0 && V.z < 0)
-        {
-            SE = true;
-        }
-        else if (V.x < 0 && V.z > 0)
-        {
-            NW = true;
-        }
-        else if (V.x < 0 && V.z < 0)
-        {
-            SW = true;
-        }
-        else {
-        }
-    }
+    //void setbools(Vector3 V)
+    //{
+    //    if (V.x > 0 && V.z > 0)
+    //    {
+    //        NE = true;
+    //    }
+    //    else if (V.x > 0 && V.z < 0)
+    //    {
+    //        SE = true;
+    //    }
+    //    else if (V.x < 0 && V.z > 0)
+    //    {
+    //        NW = true;
+    //    }
+    //    else if (V.x < 0 && V.z < 0)
+    //    {
+    //        SW = true;
+    //    }
+    //    else {
+    //    }
+    //}
     void Update () {
-		
+        if (POWER)
+        {
+            //Vector3 front = GetComponent<Rigidbody>().transform.Find("Front").position - transform.position;
+            
+        }
 	}
 }
