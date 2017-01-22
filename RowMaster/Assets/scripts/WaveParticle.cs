5fc953790e97ecbccc860fee5472923d099c8275
xEmptyPockets <mcrober1@uci.edu>
2017-01-21 17:46:29 -0800

    Made Framework for Wave Particles
    Added a script for particles, contains info like origin, force to be
    applied, etc.

---
+++ RowMaster/Assets/scripts/WaveParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveParticle : MonoBehaviour {	
+	private Vector3 origin;
+	public int id;
+	public float force;
+	public float radius;
+	public float upwardBumpForce;
+
+	void Start () {
+		origin = transform.position;
+	}
+
+	void OnParticleCollision(GameObject other){
+
+	}
+
+	void Update () {
+	}
+}
