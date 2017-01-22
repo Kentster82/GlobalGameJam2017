f35b17d91b4119c878e930d1ff445d3964fbd8f1
xEmptyPockets <mcrober1@uci.edu>
2017-01-22 15:58:16 -0800

    Ran into control bug, testing separate computer

--- RowMaster/Assets/scripts/row_row_row_ya_boat.cs
+++ RowMaster/Assets/scripts/row_row_row_ya_boat.cs
@@ -24,7 +24,7 @@ public class row_row_row_ya_boat : MonoBehaviour {
     // Use this for initialization
     void Start () {
         OldTriggerStateR =false;
-        GameObject.Find("Cheers").GetComponent<AudioSource>().Pause();
+        //GameObject.Find("Cheers").GetComponent<AudioSource>().Pause();
         switch (JoyNum)
         {
             case "Joystick1":
@@ -175,8 +175,8 @@ public class row_row_row_ya_boat : MonoBehaviour {
 						flagged = true;
 						collision.gameObject.GetComponent<row_row_row_ya_boat>().flagged = false;
                         this.transform.FindChild("WaveGenerator").GetComponent<ParticleSystem>().Emit(150);
-                        GameObject.Find("Cheers").GetComponent<AudioSource>().Play();
-						GameObject.Find("SplishSplashTakinABath").GetComponent<AudioSource>().Play();
+                        //GameObject.Find("Cheers").GetComponent<AudioSource>().Play();
+						//GameObject.Find("SplishSplashTakinABath").GetComponent<AudioSource>().Play();
                         //Debug.Log("I transfered shit");
                     }
 				}
